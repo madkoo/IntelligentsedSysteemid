16130d57103fe26ec9bfa0bc9ee721b0b9668b48
kevlaa <kevlaa@ttu.ee>
2018-03-14 16:42:38 +0200

    shop class added

---
+++ shop.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 16:11:02 2018
+
+@author: WhiteH2t
+"""
+
+class shop():
+    
+    def __init__(product_price, transport, transport_price, 
+                 transport_time, parking, parking_charge, service_language, wheelchair_accessibility, crowd):
+        self.product_price
+        self.transport
+        self.transport_price
+        self.transport_time
+        self.parking
+        self.parking_charge
+        self.service_language
+        self.wheelchair_accessibility
+        self.crowd
+        
+        
+
+        
+        
+  
+    
